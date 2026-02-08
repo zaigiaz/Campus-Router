1a92e76675cf77c6aa672882b68204598b27208d
zaigiaz <shayne.tman@gmail.com>
2026-02-08 14:43:30 -0600

    better visualization, converted to undirected graph

--- src/test.py
+++ src/test.py
@@ -1,6 +1,10 @@
 import networkx as nx
 
-g = nx.read_gml("../img/finished_nodes.gml")
+g = nx.read_gexf("../img/campus.gexf")
 
 print(len(g.nodes()))
 print(len(g.edges()))
+
+G = nx.to_undirected(g)
+
+nx.write_gexf(G, "../img/undirected.gexf")
